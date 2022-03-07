c220164d610def59236191aa9adbff3190f39d57
JS <jseb.minet@gmail.com>
2022-03-07 12:35:00 +0100

    Fix entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,11 +8,12 @@ case "$CMD" in
   package|install)
   shift 1
     CMD=(
-    ./mvnw "$CMD" \
+    ./mvnw \
+    "$MAVEN_ARGS" \
+    "$CMD" \
     -DskipTests \
     -P"$MAVEN_PROFILE" \
-    -pl '"$MAVEN_PROJECT"' \
-    "$MAVEN_ARGS"
+    -pl"$MAVEN_PROJECT" \
     "$@"
     )
     ;;
