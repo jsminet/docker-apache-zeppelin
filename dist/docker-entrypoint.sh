0df0d83f8c9991e22551ed8e088eac3919b6de5b
JS <jseb.minet@gmail.com>
2022-03-09 21:59:59 +0100

    Fix quote typo

--- dist/docker-entrypoint.sh
+++ dist/docker-entrypoint.sh
@@ -6,13 +6,13 @@ set -ex
 CMD="$1"
 
 if [[ -z "${MAVEN_PROFILE}" ]]; then
-  ALL_MAVEN_PROFILES=
+  unset ALL_MAVEN_PROFILES
 else
   ALL_MAVEN_PROFILES="-P${MAVEN_PROFILE}"
 fi
 
 if [[ -z "${MAVEN_PROJECT}" ]]; then
-  ALL_MAVEN_PROJECTS=
+  unset ALL_MAVEN_PROJECTS
 else
   ALL_MAVEN_PROJECTS="-pl '${MAVEN_PROJECT}'"
 fi
@@ -22,12 +22,12 @@ case "$CMD" in
   shift 1
     CMD=(
     ./mvnw \
-    "$MAVEN_ARGS" \
-    "$CMD" \
+    $MAVEN_ARGS \
+    $CMD \
     -DskipTests \
-    "$ALL_MAVEN_PROFILES" \
-    "$ALL_MAVEN_PROJECTS" \
-    "$@"
+    $ALL_MAVEN_PROFILES \
+    $ALL_MAVEN_PROJECTS \
+    $@
     )
     ;;
 
